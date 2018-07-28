8932bfd8c38a744003240ba018b2214931ba92b8
tiago <si.tiagoferreira@gmail.com>
2018-07-28 14:23:30 -0300

    change main to receive args parameters

--- src/main/java/br/com/mio/di/bao/Main.java
+++ src/main/java/br/com/mio/di/bao/Main.java
@@ -5,7 +5,10 @@ import br.com.mio.di.bao.business.DishBusiness;
 public class Main {
 
     public static void main(String[] args) {
-        String input = "night, 1, 2, 2, 4";
+        String input = "";
+        for(String a : args) {
+            input += a;
+        }
         DishBusiness createDishes = new DishBusiness();
         String resp = createDishes.createDishes(input);
         System.out.println(resp);
